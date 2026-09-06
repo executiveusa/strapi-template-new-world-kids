b248b942a25083bff8dd6f27c07fed8dfec0c1f9
THE PAULI EFFECT <executiveusa@gmail.com>
2026-09-05 23:28:04 -0600

    Finish homepage subtraction

--- apps/ui/src/components/homepage/Homepage.tsx
+++ apps/ui/src/components/homepage/Homepage.tsx
@@ -1,17 +1,11 @@
 import { NonprofitHero } from "./NonprofitHero"
 import { PathwaySection } from "./PathwaySection"
-import { StickyDonateMobile } from "./StickyDonateMobile"
-import { SupportSection } from "./SupportSection"
 
 export function Homepage() {
   return (
-    <>
-      <main>
-        <NonprofitHero />
-        <PathwaySection />
-        <SupportSection />
-      </main>
-      <StickyDonateMobile />
-    </>
+    <main>
+      <NonprofitHero />
+      <PathwaySection />
+    </main>
   )
 }
